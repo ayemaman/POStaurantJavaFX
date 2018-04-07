eaa7c80bb135e58d8d99f432effb3b207f160a63
ayemaman <aleksandrs.gurcenko@gmail.com>
2018-04-07 15:41:39 +0100

    MANAGER/USERcontrol more stuff

--- src/main/java/postaurant/NewUserScreenController.java
+++ src/main/java/postaurant/NewUserScreenController.java
@@ -1,14 +1,18 @@
 package postaurant;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -22,16 +26,18 @@ import postaurant.exception.InputValidationException;
 import postaurant.model.User;
 import postaurant.service.UserService;
 import java.net.URL;
-;
+
 
 
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class NewUserScreenController {
 
-    private boolean wasUserSaved=false;
-    private User user=null;
-
+    private StringProperty name = new SimpleStringProperty("");
+    private StringProperty surname = new SimpleStringProperty("");
+    private TextField currentTextField;
+    private boolean wasUserSaved = false;
+    private User user = null;
 
 
     private FXMLoaderService fxmLoaderService;
@@ -44,33 +50,42 @@ public class NewUserScreenController {
     private Button goBackButton;
     @FXML
     private Button saveUserButton;
-    @FXML private TextField nameField;
-    @FXML private TextField surnameField;
-    @FXML private ChoiceBox<String> choicePosition;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField surnameField;
+    @FXML
+    private ChoiceBox<String> choicePosition;
 
 
-    public NewUserScreenController(FXMLoaderService fxmLoaderService, UserService userService){
-        this.fxmLoaderService=fxmLoaderService;
-        this.userService=userService;
+    public NewUserScreenController(FXMLoaderService fxmLoaderService, UserService userService) {
+        this.fxmLoaderService = fxmLoaderService;
+        this.userService = userService;
     }
 
-    public void initialize(){
-        ObservableList<String> positionList= FXCollections.observableArrayList("DUBDUB", "MANAGER");
+
+    public void initialize() {
+        nameField.textProperty().bind(name);
+        surnameField.textProperty().bind(surname);
+        ObservableList<String> positionList = FXCollections.observableArrayList("DUBDUB", "MANAGER");
         choicePosition.setValue("DUBDUB");
         choicePosition.setItems(positionList);
 
-        goBackButton.setOnAction(e-> {
+        goBackButton.setOnAction(e -> {
             exitWindow(goBackButton);
             goBackButton.getScene().getWindow().hide();
         });
 
-        saveUserButton.setOnAction(e->{
+        nameField.setOnMouseClicked(e -> this.currentTextField = nameField);
+        surnameField.setOnMouseClicked(e -> this.currentTextField = surnameField);
+
+        saveUserButton.setOnAction(e -> {
             try {
                 user = new User(nameField.getText(), surnameField.getText(), choicePosition.getValue());
                 userService.saveNewUser(this.user);
                 exitWindow(saveUserButton);
                 goBackButton.getScene().getWindow().hide();
-            }catch (InputValidationException wrongUserData){
+            } catch (InputValidationException wrongUserData) {
                 try {
                     URL url = wrongInputForm.getURL();
                     Parent root = fxmLoaderService.getLoader(url).load();
@@ -81,7 +96,7 @@ public class NewUserScreenController {
                     stage.initStyle(StageStyle.UNDECORATED);
                     stage.setScene(scene);
                     stage.showAndWait();
-                }catch (Exception e1){
+                } catch (Exception e1) {
                     e1.printStackTrace();
                 }
             }
@@ -89,23 +104,58 @@ public class NewUserScreenController {
         });
     }
 
-    public void setWasUserSaved(boolean wasUserSaved){
-        this.wasUserSaved=wasUserSaved;
+    public void setWasUserSaved(boolean wasUserSaved) {
+        this.wasUserSaved = wasUserSaved;
     }
 
-    public boolean getWasUserSaved(){
+    public boolean getWasUserSaved() {
         return this.wasUserSaved;
     }
 
-    private void exitWindow(Button button){
-        if(button.getId().equals("goBackButton")){
+    private void exitWindow(Button button) {
+        if (button.getId().equals("goBackButton")) {
             setWasUserSaved(false);
-        }else if(button.getId().equals("saveUserButton")){
+        } else if (button.getId().equals("saveUserButton")) {
             setWasUserSaved(true);
         }
 
     }
+
+    public void onKeyboardPress(ActionEvent event) {
+        StringProperty stringProperty = null;
+        if (currentTextField.equals(nameField)) {
+            stringProperty = name;
+        } else if (currentTextField.equals(surnameField)) {
+            stringProperty = surname;
+        }
+        if (stringProperty != null) {
+            Button button = (Button) event.getSource();
+            switch (button.getText()) {
+                case "<--":
+                    stringProperty.set(stringProperty.getValue().substring(0, stringProperty.getValue().length() - 1));
+                    break;
+                default:
+                    stringProperty.set(stringProperty.getValue() + button.getText());
+                    break;
+
+            }
+        }
+    }
+}
 /*
+private void buttonAction(Button button) {
+        if (button.getText().equals("DELETE")) {
+            buffer.setValue("");
+        } else if (button.getText().equals("EXIT")) {
+            button.getScene().getWindow().hide();
+        } else {
+            if (buffer.getValue().length() < 5) {
+                String string = button.getText();
+                buffer.set(buffer.getValue() + string);
+            }
+        }
+
+
     public void onKeyBoardButtonClick(ActionEvent event, TextField textField){
         Button button=(Button) event.getSource();
         String buffer=button.getText();
@@ -116,4 +166,4 @@ public class NewUserScreenController {
     }
 */
 
-}
+
